8f6bb27c2cd9d02c83ef4b58ea2334410aadc86a
Keerthi kiran M <85945579+khulk17@users.noreply.github.com>
2023-12-04 15:56:50 +0530

    with new css

--- app.py
+++ app.py
@@ -2,9 +2,36 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+JOBS=[
+    {
+        "id":1,
+        "title":'Data Analyst',
+        "Location":"Bengaluru,India",
+        "Salary":"Rs :10,00,000"
+    },
+    {
+        "id":2,
+        "title":'Data Scientist',
+        "Location":"Delhi,India",
+        "Salary":"Rs: 10,50,000"
+    },
+    {
+        "id":3,
+        "title":'Frontend Devloper',
+        "Location":"Bengaluru,India",
+        "Salary":"Rs: 12,00,000"
+    },
+    {
+        "id":4,
+        "title":'Backend Developer',
+        "Location":"San Fransisco,USA",
+        "Salary":"$120,000"
+    },
+]
+
 @app.route('/')
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html",jobs=JOBS)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',debug=True)
